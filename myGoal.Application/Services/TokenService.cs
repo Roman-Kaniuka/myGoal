2226e58030d9d797878318ecdc9ed9c270e004fb
Kaniuka <kaniukaroma@gmail.com>
2024-05-21 23:30:58 +0300

    Fixed a bug when calling the RefreshToken method.

--- myGoal.Application/Services/TokenService.cs
+++ myGoal.Application/Services/TokenService.cs
@@ -57,7 +57,9 @@ public class TokenService : ITokenService
             ValidateIssuer = true,
             ValidateIssuerSigningKey = true,
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtKey)),
-            ValidateLifetime = true
+            ValidateLifetime = true,
+            ValidAudience = _audience,
+            ValidIssuer = _issuer
         };
         var tokenHandler = new JwtSecurityTokenHandler();
         var claimsPrincipal = tokenHandler.ValidateToken(accessToken, tokenValidationParameters, out var securityToken);
